5cd68d0b8b15d679cadf4332b44c7ad95ecb7701
陈龙明 <chenlongmingob@outlook.com>
2025-08-13 13:14:06 +0800

    support the latest version of pytorch (2.8.0)

--- RealESRGAN/model.py
+++ RealESRGAN/model.py
@@ -1,10 +1,8 @@
 import os
 import torch
-from torch.nn import functional as F
 from PIL import Image
 import numpy as np
-import cv2
-from huggingface_hub import hf_hub_url, cached_download
+import huggingface_hub
 
 from .rrdbnet_arch import RRDBNet
 from .utils import pad_reflect, split_image_into_overlapping_patches, stich_together, \
@@ -42,8 +40,7 @@ class RealESRGAN:
             config = HF_MODELS[self.scale]
             cache_dir = os.path.dirname(model_path)
             local_filename = os.path.basename(model_path)
-            config_file_url = hf_hub_url(repo_id=config['repo_id'], filename=config['filename'])
-            cached_download(config_file_url, cache_dir=cache_dir, force_filename=local_filename)
+            huggingface_hub.hf_hub_download(repo_id=config['repo_id'], filename=config['filename'], local_dir=cache_dir)
             print('Weights downloaded to:', os.path.join(cache_dir, local_filename))
         
         loadnet = torch.load(model_path)
@@ -56,35 +53,35 @@ class RealESRGAN:
         self.model.eval()
         self.model.to(self.device)
         
-    @torch.cuda.amp.autocast()
     def predict(self, lr_image, batch_size=4, patches_size=192,
                 padding=24, pad_size=15):
-        scale = self.scale
-        device = self.device
-        lr_image = np.array(lr_image)
-        lr_image = pad_reflect(lr_image, pad_size)
+        with torch.amp.autocast(self.device.type, dtype=torch.bfloat16):
+            scale = self.scale
+            device = self.device
+            lr_image = np.array(lr_image)
+            lr_image = pad_reflect(lr_image, pad_size)
 
-        patches, p_shape = split_image_into_overlapping_patches(
-            lr_image, patch_size=patches_size, padding_size=padding
-        )
-        img = torch.FloatTensor(patches/255).permute((0,3,1,2)).to(device).detach()
+            patches, p_shape = split_image_into_overlapping_patches(
+                lr_image, patch_size=patches_size, padding_size=padding
+            )
+            img = torch.FloatTensor(patches/255).permute((0,3,1,2)).to(device).detach()
 
-        with torch.no_grad():
-            res = self.model(img[0:batch_size])
-            for i in range(batch_size, img.shape[0], batch_size):
-                res = torch.cat((res, self.model(img[i:i+batch_size])), 0)
+            with torch.no_grad():
+                res = self.model(img[0:batch_size])
+                for i in range(batch_size, img.shape[0], batch_size):
+                    res = torch.cat((res, self.model(img[i:i+batch_size])), 0)
 
-        sr_image = res.permute((0,2,3,1)).clamp_(0, 1).cpu()
-        np_sr_image = sr_image.numpy()
+            sr_image = res.permute((0,2,3,1)).clamp_(0, 1).to(dtype=torch.float32).cpu()
+            np_sr_image = sr_image.numpy()
 
-        padded_size_scaled = tuple(np.multiply(p_shape[0:2], scale)) + (3,)
-        scaled_image_shape = tuple(np.multiply(lr_image.shape[0:2], scale)) + (3,)
-        np_sr_image = stich_together(
-            np_sr_image, padded_image_shape=padded_size_scaled, 
-            target_shape=scaled_image_shape, padding_size=padding * scale
-        )
-        sr_img = (np_sr_image*255).astype(np.uint8)
-        sr_img = unpad_image(sr_img, pad_size*scale)
-        sr_img = Image.fromarray(sr_img)
+            padded_size_scaled = tuple(np.multiply(p_shape[0:2], scale)) + (3,)
+            scaled_image_shape = tuple(np.multiply(lr_image.shape[0:2], scale)) + (3,)
+            np_sr_image = stich_together(
+                np_sr_image, padded_image_shape=padded_size_scaled, 
+                target_shape=scaled_image_shape, padding_size=padding * scale
+            )
+            sr_img = (np_sr_image*255).astype(np.uint8)
+            sr_img = unpad_image(sr_img, pad_size*scale)
+            sr_img = Image.fromarray(sr_img)
 
         return sr_img
--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import os 
+import os
 import torch
 from PIL import Image
-import numpy as np
 from RealESRGAN import RealESRGAN
+from tqdm import tqdm
 
 
 def main() -> int:
+    scale = 4
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = RealESRGAN(device, scale=4)
+    model = RealESRGAN(device, scale=scale)
     model.load_weights('weights/RealESRGAN_x4.pth', download=True)
-    for i, image in enumerate(os.listdir("inputs")):
+    for i, image in tqdm(enumerate(os.listdir("inputs"))):
         image = Image.open(f"inputs/{image}").convert('RGB')
         sr_image = model.predict(image)
-        sr_image.save(f'results/{i}.png')
-
+        sr_image.save(f'results/{i}_x{scale}.png')
+        
 
 if __name__ == '__main__':
     main()
